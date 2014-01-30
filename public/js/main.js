9e6a8cf92ea31559a002baa8a4a8f7281825f769
Stephen Peery <smp4488@gmail.com>
2014-01-30 11:49:25 -0500

    AMD library loading: tire($), underscore(_), backbone(Backbone)

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+head.load([
+	{"tire" : "js/tire-1.3.1.js"},
+	{"underscore" : "js/underscore.js"},
+	{"backbone" : "js/backbone.js"}
+]);
+
+head.ready(['tire','backbone'], function () {
+    // some callback stuff
+    console.log("App Init!!");
+    console.log("Tire: ", $);
+    console.log("Underscore: ", _);
+    console.log("backbone: ", Backbone);
+});
