21365be04986d6d9fe97b0061006dc4c73bef561
Nick Moorman <nick@nickmoorman.com>
2014-01-30 11:54:07 -0800

    Added super basic consumption of Twitter Streaming API

--- app/routes/sandbox.js
+++ app/routes/sandbox.js
@@ -4,11 +4,28 @@
 
 var http = require('http');
 var request = require('request');
+var ntwitter = require('ntwitter');
 
 exports.sandbox = function(req, res) {
   request('http://prod-tr-api-lb01.ec2.cbsi.com/api/content/article/?edition=us&fields=id%2Cheadline&limit=5', function(error, response, body) {
     if (!error && response.statusCode == 200) {
       res.send(body);
     }
+  });
+};
+
+
+exports.twitter = function(req, res) {
+  var twitter = new ntwitter({
+    consumer_key: 'bViLt6w2D1bQlWfUHb7A',
+    consumer_secret: 'aI3Lt0VHXd60Wq9RHKBYx01hSfMjDMZwuOx8NVGQDc',
+    access_token_key: '7061612-MUc8DhtFtjyxB6BGxgVOZZGiGifQ1vlpmdl5xGKquj',
+    access_token_secret: '54u3yLbz9bCMh8CEun9j5PBkQLmPzV7R9ElYzLPgbdGos'
+  });
+
+  twitter.stream('statuses/sample', function(stream) {
+    stream.on('data', function(data) {
+      console.log(data);
+    });
   });
 };
