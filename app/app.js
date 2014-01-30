1e45c3d83f5ea7b9d20ef90ba638c6947115c4af
Nick Moorman <nick@nickmoorman.com>
2014-01-30 13:12:04 -0800

    Set up 'server' in app root and added very basic Socket.IO integration

--- app/app.js
+++ app/app.js
@@ -4,14 +4,19 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var sandbox = require('./routes/sandbox');
 var http = require('http');
 var path = require('path');
+var ntwitter = require('ntwitter');
+var sockio = require('socket.io');
 
 var app = express();
+var server = http.createServer(app);
+var io = sockio.listen(server);
 
-// all environments
+// ----------------------------------------------------------------------------
+// Express app setup
+// ----------------------------------------------------------------------------
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -28,11 +33,43 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// ----------------------------------------------------------------------------
+// Routes
+// ----------------------------------------------------------------------------
 app.get('/', routes.index);
-app.get('/users', user.list);
+// NOTE: will probably move this to index route
+app.get('/client', function(req, res) {
+  res.sendfile(__dirname + '/views/client.html');
+});
 app.get('/sandbox', sandbox.sandbox);
 app.get('/sandbox/twitter', sandbox.twitter);
 
-http.createServer(app).listen(app.get('port'), function(){
+// ----------------------------------------------------------------------------
+// Initial Socket.IO stuff
+// ----------------------------------------------------------------------------
+io.sockets.on('connection', function(socket) {
+  socket.emit('data', 'connected!');
+});
+
+// ----------------------------------------------------------------------------
+// Twitter streaming API consumer, our app's "server"
+// ----------------------------------------------------------------------------
+var twitter = new ntwitter(require('./credentials.js').credentials);
+
+var filters = {
+  'track': ['cbs', 'cbs interactive', 'cnet', 'techrepublic', 'smartplanet', 'tech pro research', 'zdnet']
+}
+
+twitter.stream('statuses/filter', filters, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log('Received tweet ' + tweet.id);
+    io.sockets.emit('data', 'Received tweet ' + tweet.id);
+  });
+});
+
+// ----------------------------------------------------------------------------
+// Initialize HTTP server
+// ----------------------------------------------------------------------------
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
