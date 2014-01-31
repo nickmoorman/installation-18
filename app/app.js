78f942a26c8363fe59c93bfd7095a0c3f249dcc1
Derek Mart <demart01@gmail.com>
2014-01-31 15:03:31 -0500

    favicon flavicon!!

--- app/app.js
+++ app/app.js
@@ -47,7 +47,7 @@ var util = new Util(argv, io);
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon(path.join(__dirname, '../public/favicon.ico')));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
